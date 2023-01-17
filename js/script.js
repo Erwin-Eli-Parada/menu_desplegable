f835ba348576883851b83cb2dce768e7a35b50d7
Eli <perwin119@gmail.com>
2023-01-17 17:21:45 -0600

    logica del selector

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const navegacion = document.querySelector('.navigation');
 menuToggle.addEventListener("click",function(){
     navegacion.classList.toggle('open')
 });
+
+const list = document.querySelectorAll(".list");
+
+list.forEach(item => {
+    item.addEventListener("click", function(){
+        list.forEach(item => {
+            item.classList.remove("active");
+        });
+        item.classList.add("active");
+    });
+});
